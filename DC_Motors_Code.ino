7e7b0617fae27926849dde8c95d09dfa2822d293
Norab99 <52720500+Norab99@users.noreply.github.com>
2020-06-21 00:20:47 +0300

    Update DC_Motors_Code.ino

--- DC_Motors_Code.ino
+++ DC_Motors_Code.ino
@@ -1,34 +1,40 @@
+#define Sw1 11  // ON & OFF Switch
+#define Sw2 12  // Change the Direction
+
+#define OM11 1  // input1 that attecned to motor1
+#define OM12 2  // input2 that attecned to motor1
+#define OM21 8  // input1 that attecned to motor2
+#define OM22 7  // input2 that attecned to motor2
 
 void setup()
 {
-  pinMode(12, INPUT_PULLUP);    // ON & OFF Switch
-  pinMode(11, INPUT_PULLUP);   // Change the Direction
-} 
+  pinMode(Sw2, INPUT_PULLUP); 
+  pinMode(Sw1, INPUT_PULLUP);  
+}
 
 void loop()
 {
-  if (digitalRead(12) == 1)    // The switch is ON
+  if (digitalRead(Sw2) == 1) // The switch is ON
   {
-    if (digitalRead(11) == 1)  // counterclockwise
-    {  
-      digitalWrite(1, 1);
-      digitalWrite(2,0);
-      digitalWrite(8, 1);
-      digitalWrite(7,0);
+    if (digitalRead(Sw1) == 1) {  // counterclockwise
+  		digitalWrite(OM11, 1);
+  		digitalWrite(OM12,0);
+  		digitalWrite(OM21, 1);
+  		digitalWrite(OM22,0);
     }
     
-    else {                     // clockwise
-      digitalWrite(1, 0);
-      digitalWrite(2,1);
-      digitalWrite(8, 0);
-      digitalWrite(7,1);
+    else {                   // clockwise
+  		digitalWrite(OM11, 0);
+  		digitalWrite(OM12,1);
+  		digitalWrite(OM21, 0);
+  		digitalWrite(OM22,1);
     }
   }
   
-  else {                    // The switch is OFF
-    digitalWrite(1, 0);
-    digitalWrite(2,0);
-    digitalWrite(8, 0);
-    digitalWrite(7,0); 
+  else {                // The switch is off
+    digitalWrite(OM11, 0);
+  	digitalWrite(OM12,0);
+  	digitalWrite(OM21, 0);
+  	digitalWrite(OM22,0); 
   }
 }
