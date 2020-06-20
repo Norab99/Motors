5b92788664b8caa4b4da5e6a86932d9b1fd561c2
Norab99 <52720500+Norab99@users.noreply.github.com>
2020-06-20 20:36:57 +0300

    Update Stepper_Motor_Code.ino

--- Stepper_Motor_Code.ino
+++ Stepper_Motor_Code.ino
@@ -1,7 +1,12 @@
 
 #include <Stepper.h>
 
-#define stepPerRevolution 32 // The angle for 4 steps sequence for 28-BYJ48 stepper motor is 11.25 degree since it is 5.625°(given in datasheet) for 8 step sequence it from datasheet, so the steps per revolution = 360/(5.625*2) = 32.
+/* The angle for 4 steps sequence for 28-BYJ48 stepper motor is 11.25 degree 
+since it is 5.625°(given in datasheet) for 8 step sequence it from datasheet, 
+so the steps per revolution = 360/(5.625*2) = 32. To make one complete rotation
+we need to move 2048 = 32*64=2048 as we have 32 steps and 64 as the gear ratio. */
+
+#define stepPerRevolution 32  
 
 Stepper StepperMotor(stepPerRevolution, 8, 10, 9, 11);
 
@@ -13,10 +18,10 @@ void setup() {
 
 void loop() {  
   if (digitalRead(3) == 0) { 
-    StepperMotor.step(stepPerRevolution);         // The motor will complate one revolution counterclockwise 
+    StepperMotor.step(stepPerRevolution);         // The motor will move counterclockwise 
   }
 
  else if (digitalRead(4) == 0) {
-    StepperMotor.step(-stepPerRevolution);      // The motor will complate half revolution clockwise 
+    StepperMotor.step(-stepPerRevolution);      // The motor will move clockwise 
   }
 }
